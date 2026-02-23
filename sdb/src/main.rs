23b17d1ebeffa8d1dd8c1b6b899661fe0fdb74a2
Paul van Vulpen <paul@paulvanvulpen.nl>
2026-02-23 22:48:47 +0100

    Update dependencies on other crates

--- sdb/src/main.rs
+++ sdb/src/main.rs
@@ -1,4 +1,48 @@
-fn main() {
-    libsdb::add(2, 4);
-    println!("Hello, world!");
+use anyhow::{Context, Result, bail};
+use clap::Parser;
+use env_logger::Env;
+
+use nix::sys::ptrace;
+use nix::unistd::{self, Pid};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg()]
+    program_path: String,
+
+    #[arg(short = 'p')]
+    pid: Option<i32>,
+}
+
+fn attach(args: &Args) -> Result<Pid> {
+    match args.pid {
+        Some(pid) => {
+            if pid <= 0 {
+                bail!("Invalid pid")
+            }
+            let pid = Pid::from_raw(pid);
+            ptrace::attach(pid).with_context(|| format!("failed to attach process {}", pid))?;
+        }
+        None => {
+            let fork_result = unsafe { unistd::fork().context("fork failed")? };
+            if fork_result.is_child() {
+                trace
+            }
+        }
+    }
+
+    // placeholder
+    Pid::from_raw(args.pid.unwrap_or(-1))
+}
+
+fn main() -> Result<()> {
+    let mut builder = env_logger::Builder::from_env(Env::default().default_filter_or("info"));
+    builder.target(env_logger::Target::Stdout);
+    builder.init();
+
+    let args = Args::parse();
+    attach(&args);
+
+    Ok(())
 }
