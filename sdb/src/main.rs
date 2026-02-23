8e2c3f4b9b4196654c2d53e09750f94ecac1db83
Paul van Vulpen <paul@paulvanvulpen.nl>
2026-02-23 22:49:03 +0100

    Produce a very basic trace function

--- sdb/src/main.rs
+++ sdb/src/main.rs
@@ -2,7 +2,7 @@ use anyhow::{Context, Result, bail};
 use clap::Parser;
 use env_logger::Env;
 
-use nix::sys::ptrace;
+use nix::sys;
 use nix::unistd::{self, Pid};
 
 #[derive(Parser, Debug)]
@@ -15,25 +15,32 @@ struct Args {
     pid: Option<i32>,
 }
 
-fn attach(args: &Args) -> Result<Pid> {
+fn attach(args: Args) -> Result<Pid> {
     match args.pid {
         Some(pid) => {
             if pid <= 0 {
                 bail!("Invalid pid")
             }
             let pid = Pid::from_raw(pid);
-            ptrace::attach(pid).with_context(|| format!("failed to attach process {}", pid))?;
+            sys::ptrace::attach(pid).with_context(|| format!("attach to process {}", pid))?;
+            Ok(pid)
         }
         None => {
             let fork_result = unsafe { unistd::fork().context("fork failed")? };
             if fork_result.is_child() {
-                trace
+                sys::ptrace::traceme()
+                    .context("allow to send more ptrace request to this process in the future")?;
+                let program_path = std::ffi::CString::new(args.program_path)
+                    .context("exec_vector_path requires a c-string")?;
+
+                unistd::execvp(&program_path, &[&program_path])?;
+                let wait_status = sys::wait::wait().context(
+                    "wait for child process to change status / has child changed status",
+                )?;
             }
+            Ok(Pid::from_raw(0))
         }
     }
-
-    // placeholder
-    Pid::from_raw(args.pid.unwrap_or(-1))
 }
 
 fn main() -> Result<()> {
@@ -42,7 +49,7 @@ fn main() -> Result<()> {
     builder.init();
 
     let args = Args::parse();
-    attach(&args);
+    attach(args);
 
     Ok(())
 }
